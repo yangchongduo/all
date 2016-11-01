ba78d1fbec9aba2f5637c9f7b97c89483047d707
Chongduo Yang <980058984@qq.com>
2016-11-01 19:01:04 +0800

    Update pm2.js

--- pm2.js
+++ pm2.js
@@ -2,8 +2,8 @@
 pm2 start app.js -i  4  这样是启动了四个进程node
 
 
-安装：npm install -g pm2
-启动程序：pm2 start <app_name|id|all>
+安装：npm install -g pm2 
+启动程序：pm2 start <app_name|id|all> -i 4 再说一次会启动 4个node进程
 列举进程：pm2 list
 退出程序：pm2 stop <app_name|id|all>
 重起应用：pm2 restart
