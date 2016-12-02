8bb7df6331a014e418ee25ca2aa312bfe1d87985
yangchongduo <yangchongduo@le.com>
2016-12-02 13:20:12 +0800

    数据库

---
+++ export/1.js
@@ -0,0 +1,11 @@
+/*
+ import {name,age} from './2.js';
+ console.log(name,age)*/
+/*var obj={name:'ycd'}
+const fn=(obj)=>({
+    ...obj,
+    pend:'1'
+});
+console.log(fn(obj))*/
+//
+
--- self/export/1.js
+++
@@ -1,2 +0,0 @@
-import {name,age} from './2.js';
-console.log(name,age)
